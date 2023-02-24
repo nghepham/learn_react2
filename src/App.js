cc669495c40deac0b82483e6365c201a0e127c22
Nghe Pham <npham@markany.com>
2023-02-24 16:52:51 +0700

    list item and responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      <header id='header'>
+        <nav class="topnav" id="myTopnav">
+          <div class="topnav-centered">
+            <a href="#home" class="active">Home</a>
+          </div>
+          <div class="topnav-right">
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+            <a href="#search">Search</a>
+          </div>
+        </nav>
       </header>
+      <main id='main'>
+        {/* <div id='header-container'>
+          <header id='header-bar'>
+            <nav id='navbar' className='top-navbar'>
+              <div id='left-navbar'>
+                <p id='logo'>
+                  PVNGHE
+                </p>
+                <p className='item'>
+                  Flashbytes
+                </p>
+                <p className='item'>
+                  Categories
+                </p>
+                <p className='item'>
+                  Upload video
+                </p>
+              </div>
+              <div id='right-navbar'>
+                <div id='search-container'>
+                  <input type={'text'} name='search-box' />
+                  <img src='search' alt='search icon' />
+                </div>
+                <img id='notification' src='https://lh3.googleusercontent.com/ogw/AAEL6sjTgpxR3yQY5uy5T9gmcU2ru0jWYX7fdueedNl7=s64-c-mo' alt='profile-pic' />
+                <img src='https://lh3.googleusercontent.com/ogw/AAEL6sjTgpxR3yQY5uy5T9gmcU2ru0jWYX7fdueedNl7=s64-c-mo' alt='profile' />
+              </div>
+            </nav>
+          </header>
+        </div> */}
+        <div id='list-container'>
+          <div id='section'>
+            <article className='card-item'>
+              <img className='img-item' src='https://picsum.photos/300/200' alt='card' />
+              <p className='card-title'>title hello world!, i'm Nghe. I can learn and work on everything in the world! Good luck!</p>
+            </article>
+            <article className='card-item'>
+              <img className='img-item' src='https://picsum.photos/300/200' alt='card' />
+              <p className='card-title'>title</p>
+            </article>
+            <article className='card-item'>
+              <img className='img-item' src='https://picsum.photos/300/200' alt='card' />
+              <p className='card-title'>title</p>
+            </article>
+            <article className='card-item'>
+              <img className='img-item' src='https://picsum.photos/300/200' alt='card' />
+              <p className='card-title'>title</p>
+            </article>
+            <article className='card-item'>
+              <img className='img-item' src='https://picsum.photos/300/200' alt='card' />
+              <p className='card-title'>title</p>
+            </article>
+            <article className='card-item'>
+              <img className='img-item' src='https://picsum.photos/300/200' alt='card' />
+              <p className='card-title'>title</p>
+            </article>
+            <article className='card-item'>
+              <img className='img-item' src='https://picsum.photos/300/200' alt='card' />
+              <p className='card-title'>title</p>
+            </article>
+            <article className='card-item'>
+              <img className='img-item' src='https://picsum.photos/300/200' alt='card' />
+              <p className='card-title'>title</p>
+            </article>
+            <article className='card-item'>
+              <img className='img-item' src='https://picsum.photos/300/200' alt='card' />
+              <p className='card-title'>title</p>
+            </article>
+            <article className='card-item'>
+              <img className='img-item' src='https://picsum.photos/300/200' alt='card' />
+              <p className='card-title'>title</p>
+            </article>
+            <article className='card-item'>
+              <img className='img-item' src='https://picsum.photos/300/200' alt='card' />
+              <p className='card-title'>title</p>
+            </article>
+            <article className='card-item'>
+              <img className='img-item' src='https://picsum.photos/300/200' alt='card' />
+              <p className='card-title'>title</p>
+            </article>
+            <article className='card-item'>
+              <img className='img-item' src='https://picsum.photos/300/200' alt='card' />
+              <p className='card-title'>title</p>
+            </article>
+            <article className='card-item'>
+              <img className='img-item' src='https://picsum.photos/300/200' alt='card' />
+              <p className='card-title'>title</p>
+            </article>
+          </div>
+
+        </div>
+      </main>
     </div>
   );
 }
