b50d13ce8f2e5df0e3d53c8eee00bf5e7eb5c0f9
Nghe Pham <npham@markany.com>
2023-02-24 20:08:31 +0700

    grid view layout

--- src/News.js
+++ src/News.js
@@ -3,8 +3,48 @@ import './App.css';
 
 export default function News() {
   return (
-    <div id='list-container'>
-      <div id='section'>
+    <div id='news-container'>
+      <div>
+        <img className='img-grid-item' src='https://picsum.photos/200/300' alt='card' />
+      </div>
+      <div>
+        <img className='img-grid-item' src='https://picsum.photos/200/300' alt='card' />
+      </div>
+      <div>
+        <img className='img-grid-item' src='https://picsum.photos/200/300' alt='card' />
+      </div>
+      <div>
+        <img className='img-grid-item' src='https://picsum.photos/200/300' alt='card' />
+      </div>
+      <div>
+        <img className='img-grid-item' src='https://picsum.photos/200/300' alt='card' />
+      </div>
+      <div>
+        <img className='img-grid-item' src='https://picsum.photos/200/300' alt='card' />
+      </div>
+      <div>
+        <img className='img-grid-item' src='https://picsum.photos/200/300' alt='card' />
+      </div>
+      <div>
+        <img className='img-grid-item' src='https://picsum.photos/200/300' alt='card' />
+      </div>
+      <div>
+        <img className='img-grid-item' src='https://picsum.photos/200/300' alt='card' />
+      </div>
+      <div>
+        <img className='img-grid-item' src='https://picsum.photos/200/300' alt='card' />
+      </div>
+      <div>
+        <img className='img-grid-item' src='https://picsum.photos/200/300' alt='card' />
+      </div>
+      <div>
+        <img className='img-grid-item' src='https://picsum.photos/200/300' alt='card' />
+      </div>
+      <div>
+        <img className='img-grid-item' src='https://picsum.photos/200/300' alt='card' />
+      </div>
+      <div>
+        <img className='img-grid-item' src='https://picsum.photos/200/300' alt='card' />
       </div>
     </div>
   )
